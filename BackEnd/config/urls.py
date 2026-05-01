f82c885f857c594d9b14ec95728a66d13aed2d0e
Andrii <dneprovskaya401@gmail.com>
2026-05-01 23:36:23 +0300

    11

--- BackEnd/config/urls.py
+++ BackEnd/config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home
+from profile.views import home
+from BackEnd.authorization.views import authorization 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('authorization/', authorization, name='authorization')
 ]
