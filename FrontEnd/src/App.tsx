27920998da467e80ec9d553d45314c70446d57ec
Andrii <dneprovskaya401@gmail.com>
2026-05-01 18:44:59 +0300

    7

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import '../src/components/page_space/page_space'
+import Page_space from '../src/components/page_space/page_space'
 
 function App() {
 
-  return 
-
+  return (
+    <Page_space/>
+  )
 }
 
 export default App
---
+++ FrontEnd/src/components/page_space/page_space.tsx
@@ -0,0 +1,11 @@
+import './page_space_style.scss';
+
+function Page_space() {
+
+    return (
+        <div className='block'/>
+        
+    )
+}
+
+export default Page_space
