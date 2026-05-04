58b9f6885a30c81073dd3184904a232e2e596115
Andrii <dneprovskaya401@gmail.com>
2026-05-04 23:01:39 +0300

    19

--- BackEnd/authorization/views.py
+++ BackEnd/authorization/views.py
@@ -12,12 +12,28 @@ def register_view(request):
         if user_form.is_valid():
             user = user_form.save()
             user.set_password(user_form.cleaned_data['password1'])
-            login(request, user_form)
-            return redirect('home')
-    else:
-        user_form = CustomUserForm()
+            user.save()
+            refresh = RefreshToken.for_user(user)
 
-    return render(request, 'authorization/register.html', {'form': user_form})
+            response = JsonResponse({
+                "access": str(refresh.access_token),
+                })
+            
+            response.set_cookie(
+                key="refresh_token",
+                value=str(refresh),
+                httponly=True,
+                secure=True,   
+                samesite="Lax",
+                path="/",
+            )
+
+            return response
+        
+        return JsonResponse({"error": "Invalid form"}, status=400)
+
+    return JsonResponse({"error": "Method not allowed"}, status=405)
+
 
 def login_view(request):
     if request.method == 'POST':
@@ -35,8 +51,6 @@ def login_view(request):
 
             if user and user.check_password(password):
                 refresh = RefreshToken.for_user(user)
-        else:
-            user_form = CustomAuthentificationForm()
 
             response = JsonResponse({
                     "access": str(refresh.access_token),
