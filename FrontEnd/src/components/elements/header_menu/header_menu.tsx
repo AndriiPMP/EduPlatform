8907f7d656f4fd4566bfcee7c9c925733c160a87
Andrii <dneprovskaya401@gmail.com>
2026-05-14 20:19:41 +0300

    43

--- FrontEnd/src/components/elements/header_menu/header_menu.tsx
+++ FrontEnd/src/components/elements/header_menu/header_menu.tsx
@@ -1,8 +1,8 @@
 import './header_menu.scss'
 import { useState } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence ,motion } from "framer-motion";
 import { FiUser } from "react-icons/fi";
-import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
 import Heater_menu_options from '../heater_menu_options/heater_menu_options';
 
 
@@ -13,17 +13,30 @@ function Header_menu(){
         <div className='block_header_menu' onClick={() => setOpen(v => !v)}> 
 
             <motion.div
-            animate={{ rotate: open ? 90 : -90 }}
+            animate={{ rotate: open ? 180 : 0 }}
             transition={{ duration: 0.25, ease: "easeInOut" }}
             >
-                <IoIosArrowBack className='heater_menu_arrow' />
+                <IoIosArrowDown className='heater_menu_arrow' />
             </motion.div>
 
             <div className="user_icon_heater">
                 <FiUser className='user_icon_figure'/>
             </div>
 
-            <Heater_menu_options className ='heater_menu_options'/>
+            <AnimatePresence>
+                {open && (
+                     <motion.div
+                        className="heater_menu_options"
+                        key="menu-options"
+                        initial={{ opacity: 0, y: 0 }}
+                        animate={{ opacity: 1, y: 0 }}
+                        exit={{ opacity: 0, y: 0 }}
+                        transition={{ duration: 0.2, ease: "easeOut" }}
+                        >
+                    <Heater_menu_options />
+                        </motion.div>
+                        )}
+            </AnimatePresence>
         </div>
     )
 }
