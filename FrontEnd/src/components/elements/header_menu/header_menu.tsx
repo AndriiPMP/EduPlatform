325fccfa886dd668313329df2fd507e382231fdd
Andrii <dneprovskaya401@gmail.com>
2026-05-14 19:31:02 +0300

    41

--- FrontEnd/src/components/elements/header_menu/header_menu.tsx
+++ FrontEnd/src/components/elements/header_menu/header_menu.tsx
@@ -1,13 +1,23 @@
 import './header_menu.scss'
+import { useState } from "react";
+import { motion } from "framer-motion";
 import { FiUser } from "react-icons/fi";
 import { IoIosArrowBack } from "react-icons/io";
 
 
 function Header_menu(){
+ const [open, setOpen] = useState(false);
 
     return (
-        <div className='block_header_menu'>
-            <IoIosArrowBack />
+        <div className='block_header_menu' onClick={() => setOpen(v => !v)}> 
+
+            <motion.div
+            animate={{ rotate: open ? 90 : -90 }}
+            transition={{ duration: 0.25, ease: "easeInOut" }}
+            >
+                <IoIosArrowBack className='heater_menu_arrow' />
+            </motion.div>
+
             <div className="user_icon_heater">
                 <FiUser className='user_icon_figure'/>
             </div>
