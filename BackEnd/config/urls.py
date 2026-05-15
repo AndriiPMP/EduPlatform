4557b02d56dd09b0d60e8cb5d7fd1f7381fde9a1
Andrii <dneprovskaya401@gmail.com>
2026-05-15 19:00:19 +0300

    48

--- BackEnd/config/urls.py
+++ BackEnd/config/urls.py
@@ -16,14 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from authorization.views import register_view, login_view, logout_view
 from scr.refresh_tok import RefreshFromCookieView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register/', register_view, name='register'),
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
+    path('', include('authorization.urls')),
     path("token/refresh/", RefreshFromCookieView.as_view(), name="token_refresh"),
 ]
