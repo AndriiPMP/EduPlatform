2f0184c0c211bcf9a1f10ea008e14df3c97e8d89
Andrii <dneprovskaya401@gmail.com>
2026-05-15 16:58:04 +0300

    44

--- FrontEnd/src/components/elements/heater_menu_options/heater_menu_options.tsx
+++ FrontEnd/src/components/elements/heater_menu_options/heater_menu_options.tsx
@@ -1,9 +1,26 @@
 import './heater_menu_options.scss'
+import type { MouseEvent } from "react";
+import { useNavigate } from "react-router-dom";
 import { FaGear } from "react-icons/fa6";
 import { FaMoon } from "react-icons/fa";
 import { MdOutlineLogout } from "react-icons/md";
 
 function Heater_menu_options({ className = "" }) {
+    const navigate = useNavigate();
+
+    const handleLogout = async (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation();
+
+    const res = await fetch("http://127.0.0.1:8000/logout/", {
+      method: "GET",
+      credentials: "include",
+    });
+
+    if (res.ok) {
+      localStorage.removeItem("access_token");
+      navigate("/login");
+    }
+  };
 
     return (
         <div className={`block_heater_menu_options ${className}`}>
@@ -17,10 +34,9 @@ function Heater_menu_options({ className = "" }) {
                 <p className='heater_menu_options_text'>dark mode</p>
             </div>
 
-            <div className='subblock_heater_menu_options'>
+            <div className='subblock_heater_menu_options' onClick={handleLogout}>
                 <MdOutlineLogout className='heater_menu_options_icon'/>
-                <p className='heater_menu_options_text'>log out
-                </p>
+                <p className='heater_menu_options_text'>log out</p>
             </div>
         </div>
     )
