31d37d61c0305a47295600ca8aa759e6614e9282
Andrii <dneprovskaya401@gmail.com>
2026-06-02 17:50:08 +0300

    49

---
+++ FrontEnd/src/components/elements/line/line.tsx
@@ -0,0 +1,9 @@
+import './line.scss'
+
+function Line() {
+    return (
+            <hr className='line'/>
+    )
+}
+
+export default Line
--- FrontEnd/src/pages/profile_page/profile_page.tsx
+++ FrontEnd/src/pages/profile_page/profile_page.tsx
@@ -1,4 +1,6 @@
 import './profile_page.scss'
+import { FiUser } from "react-icons/fi";
+import Line from '../../components/elements/line/line'
 import Page_space from '../../components/page_space/page_space'
 import Header_menu from '../../components/elements/header_menu/header_menu'
 
@@ -7,10 +9,42 @@ function Profile_page() {
     return (            
         <Page_space>
             <Header_menu className='heater_profile_page'/>
-            <div className='block1'>
-                <input
-                type="text"
-                />
+
+            <div className='profile_block_column'>
+                <div className='profile_icon_block'>
+                    <FiUser className='user_icon_heater'/>
+                    <div className='profile_icon_edit'/>
+                </div>
+                
+                <div className='block1'>
+                    <div className='profile_page_subblock_input'>
+                        <p className='profile_page_p'>Name</p>
+                        <input
+                        className='profile_page_input'
+                        type="text"
+                        />
+                    </div>
+                    <Line/>
+
+
+                    <div className='profile_page_subblock_input'>
+                        <p className='profile_page_p'>Email</p>
+                        <input
+                        className='profile_page_input'
+                        type="text"
+                        />
+                    </div>
+                    <Line/>
+
+                    <div className='profile_page_subblock_input'>
+                        <p className='profile_page_p'>Password</p>
+                        <input
+                        className='profile_page_input'
+                        type="text"
+                        />
+                    </div>
+                    <Line/>
+                </div>
             </div>
         </Page_space>
     )
