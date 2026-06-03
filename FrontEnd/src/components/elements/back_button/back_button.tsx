75b7cb3b23181fc362d303263c0f05a7426b2374
Andrii <dneprovskaya401@gmail.com>
2026-06-03 22:51:35 +0300

    53

--- FrontEnd/src/components/elements/back_button/back_button.tsx
+++ FrontEnd/src/components/elements/back_button/back_button.tsx
@@ -1,14 +1,14 @@
 import './back_button.scss'
-import type { ReactNode } from 'react';
+import type { ReactNode, HTMLAttributes } from 'react';
 import { FaArrowLeft } from "react-icons/fa";
 
 type Props = {
   children: ReactNode;
-};
+} & HTMLAttributes<HTMLDivElement>;;
 
-function Back_button({children}: Props) {
+function Back_button({children, ...props}: Props) {
     return (
-        <div className='return_block'>
+        <div className='return_block' {...props}>
             <FaArrowLeft className='profile_arrow'/>
             {children}
         </div>
--- FrontEnd/src/pages/profile_page/profile_page.tsx
+++ FrontEnd/src/pages/profile_page/profile_page.tsx
@@ -1,17 +1,19 @@
 import './profile_page.scss'
 import { FiUser } from "react-icons/fi";
+import { useNavigate } from 'react-router-dom';
 import Back_button from '../../components/elements/back_button/back_button';
 import Line from '../../components/elements/line/line'
 import Page_space from '../../components/page_space/page_space'
 import Header_menu from '../../components/elements/header_menu/header_menu'
 
 function Profile_page() {
+    const navigate = useNavigate();
 
     return (            
         <Page_space>
             <Header_menu className='heater_profile_page'/>
 
-            <Back_button>
+            <Back_button onClick={() => navigate('/')}>
                 <p className='profile_return_p'>Profile Settings</p>   
             </Back_button>
 
