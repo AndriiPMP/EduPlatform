1a4c3f4685b805a72db0329e39f10159b19fab1b
Andrii <dneprovskaya401@gmail.com>
2026-06-10 14:48:47 +0300

    65

---
+++ FrontEnd/src/components/elements/card/card.tsx
@@ -0,0 +1,11 @@
+import './card.scss';
+
+function Card() {
+    return (
+        <div className='card_block'>
+            
+        </div>
+    )
+}
+
+export default Card
--- FrontEnd/src/pages/materials_page/materials_page.tsx
+++ FrontEnd/src/pages/materials_page/materials_page.tsx
@@ -1,6 +1,9 @@
+import './materials_page.scss'
 import Page_space from "../../components/page_space/page_space"
 import Header_menu from "../../components/elements/header_menu/header_menu"
 import SideBar from "../../components/elements/side_bar/side_bar"
+import Card from '../../components/elements/card/card'
+import Line from '../../components/elements/line/line'
 
 
 function MaterialsPage() {
@@ -9,6 +12,9 @@ function MaterialsPage() {
             <SideBar/>
             <Page_space>
                 <Header_menu className='heater_profile_page'/>
+                <p className="materials_p">Materials</p>
+                <Line/>
+                <Card/>
             </Page_space>
         </div>
     )
