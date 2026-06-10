e5ab3cd5538258b3653de12bcae89ae32d1ce1b1
Andrii <dneprovskaya401@gmail.com>
2026-06-10 15:41:07 +0300

    68

--- FrontEnd/src/components/elements/card/card.tsx
+++ FrontEnd/src/components/elements/card/card.tsx
@@ -1,10 +1,12 @@
 import './card.scss';
+import { FaPlus } from "react-icons/fa6";
+
 
 function Card() {
     return (
         <div className='card_block'>
             <div className='card_subblock'>
-                
+                <FaPlus className='card_plus'/>
             </div>
         </div>
     )
