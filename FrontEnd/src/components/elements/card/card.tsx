7f3d87f94234d764a3a158ea16ace8e2ef96be0c
Andrii <dneprovskaya401@gmail.com>
2026-06-22 23:22:34 +0300

    93

--- FrontEnd/src/components/elements/card/card.tsx
+++ FrontEnd/src/components/elements/card/card.tsx
@@ -3,18 +3,27 @@ import New_lesson from '../../windows/new_lesson/new_lesson';
 import { useState } from 'react';
 import { FaPlus } from "react-icons/fa6";
 
+interface CardProps {
+    title: string,
+    cover: string | null
+}
 
-function Card() {
+function Card({title = 'New Course'}, cover: CardProps) {
     const [open, setOpen] = useState(false);
 
     return (
-        <div>
+        <div className='card_main_block'>
             <div  className='card_block' onClick={() => setOpen(true)}>
-                <div className='card_subblock'>
-                    <FaPlus className='card_plus'/>
-                </div>
+                {cover ? (
+                    <img src={cover}/>
+                ) : (
+                    <div className='card_subblock'>
+                        <FaPlus className='card_plus'/>
+                    </div>
+            )}
+
             </div>
-            
+            <p className='card_name'>{title}</p>
             {open && <New_lesson onClose={() => setOpen(false)} />}
         </div>
     )
