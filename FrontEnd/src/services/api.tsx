129aad8e1e0a3cc87b0c8d88c862d3410c0a727e
Andrii <dneprovskaya401@gmail.com>
2026-06-27 00:00:40 +0300

    103

--- FrontEnd/src/services/api.tsx
+++ FrontEnd/src/services/api.tsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 
 const api = axios.create({
     baseURL: import.meta.env.BASE_URL,
@@ -6,6 +8,7 @@ const api = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
+    withCredentials: true,
 })
 
 export default api
@@ -25,3 +28,32 @@ api.interceptors.request.use(
         return Promise.reject(error);
     }
 );
+
+api.interceptors.response.use(
+    (response) => response,
+
+    async (error) => {
+        const originalRequest = error.config;
+        const navigate = useNavigate();
+        
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+
+        try{ 
+            const { data } = await axios.post(
+                `${import.meta.env.VITE_BASE_URL}/token/refresh/`,
+                {},
+                { withCredentials: true}
+            );
+            localStorage.setItem('access_token', data.acess);
+            originalRequest.headers.Authorization = `Bearer ${data.acess}`;
+            return api(originalRequest);
+        } catch (refreshError) {
+            console.error('Refresh failed', refreshError);
+            localStorage.removeItem('accessToken');
+            navigate('/login');
+            return Promise.reject(refreshError);
+        }
+        }
+    }
+)
